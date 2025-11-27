fb6428b3eb551846dfbe5437fc31a6e84be362aa
Scollao <90000279+Scollao@users.noreply.github.com>
2025-11-27 20:30:12 -0300

    Remove allowMultipleMainInstances option
    Removed the option to allow multiple main instances.

--- config.js
+++ config.js
@@ -4,5 +4,4 @@ Protobject.initialize(
         { name: "Phone", page: "phone.html" },
         { name: "Graph", page: "NFL_Teams_Chart4.html", main: true },
     ],
-    { allowMultipleMainInstances: true }
 );
